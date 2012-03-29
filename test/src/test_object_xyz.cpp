5e39f1d09545aa7887ae04a1d5eca0cea089f1bb
Janus Heide <janusheide@gmail.com>
2012-03-29 14:04:26 +0200

    APS->ApS

--- test/src/test_object_xyz.cpp
+++ test/src/test_object_xyz.cpp
@@ -1,4 +1,4 @@
-// Copyright Steinwurf APS 2011-2012.
+// Copyright Steinwurf ApS 2011-2012.
 // Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
 // See accompanying file LICENSE.rst or
 // http://www.steinwurf.com/licensing
