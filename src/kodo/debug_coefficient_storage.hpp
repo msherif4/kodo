ce43a2aaf53ebf10720b1a5dce10f910a686b77b
chres <cwso08@student.aau.dk>
2013-04-14 03:36:16 +0200

    Added new debug layers

---
+++ src/kodo/debug_coefficient_storage.hpp
@@ -0,0 +1,83 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include <cstdint>
+
+#include <fifi/fifi_utils.hpp>
+
+namespace kodo
+{
+    /// @ingroup debug
+    /// @ingroup coefficient_storage_layers
+    ///
+    /// @brief Print functions for coefficient storage
+    ///
+    /// This layer implements useful functions to print stored coefficients
+    template<class SuperCoder>
+    class debug_coefficient_storage : public SuperCoder
+    {
+    public:
+
+        /// @copydoc layer::field_type
+        typedef typename SuperCoder::field_type field_type;
+
+        /// @copydoc layer::value_type
+        typedef typename field_type::value_type value_type;
+
+    public:
+
+        /// Prints the decoding matrix to the output stream
+        /// @param out The output stream to print to
+        void print_decoding_matrix(std::ostream& out)
+        {
+            for(uint32_t i = 0; i < SuperCoder::symbols(); ++i)
+            {
+                /// @todo uncomment this if API implements functions to
+                ///       access uncoded/coded coefficients
+                //if( m_uncoded[i] )
+                //{
+                //    out << i << " U:\t";
+                //}
+                //else if( m_coded[i] )
+                //{
+                //    out << i << " C:\t";
+                //}
+                //else
+                //{
+                //    out << i << " ?:\t";
+                //}
+
+                print_coefficients(out, i);
+            }
+
+            out << std::endl;
+        }
+
+        /// Prints a vector of coefficients
+        /// @param out The output stream to print to
+        /// @param coefficients_index The index of the coefficients vector
+        ///                           to print
+        void print_coefficients(std::ostream& out, uint32_t coefficients_index)
+        {
+            value_type* coefficients_i =
+                SuperCoder::coefficients_value(coefficients_index);
+
+            for(uint32_t j = SuperCoder::symbols(); j > 0; --j)
+            {
+                value_type value =
+                    fifi::get_value<field_type>(coefficients_i, j-1 );
+
+                out << (uint32_t)value << " ";
+            }
+
+            out << std::endl;
+        }
+
+    };
+
+}
+
---
+++ src/kodo/debug_symbol_storage.hpp
@@ -0,0 +1,101 @@
+// Copyright Steinwurf ApS 2011-2013.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include <cstdint>
+
+#include <fifi/fifi_utils.hpp>
+
+namespace kodo
+{
+
+    /// @ingroup debug
+    /// @ingroup symbol_storage_layers
+    ///
+    /// @brief Print functions for symbol storage.
+    ///
+    /// This layer implements useful functions to print stored symbols.
+    template<class SuperCoder>
+    class debug_symbol_storage : public SuperCoder
+    {
+    public:
+
+        /// @copydoc layer::field_type
+        typedef typename SuperCoder::field_type field_type;
+
+        /// @copydoc layer::value_type
+        typedef typename field_type::value_type value_type;
+
+    public:
+
+        /// Prints the symbol matrix to the output stream
+        /// @param out The output stream to print to
+        void print_symbol_matrix(std::ostream& out)
+        {
+            uint32_t symbol_elements =
+                fifi::length_to_elements<field_type>(
+                    SuperCoder::symbol_length());
+
+            print_symbol_matrix(out, symbol_elements);
+        }
+
+        /// Prints the symbol matrix to the output stream
+        /// @param out The output stream to print to
+        /// @param symbol_elements number of elements in a symbol
+        void print_symbol_matrix(std::ostream& out, uint32_t symbol_elements)
+        {
+            assert(symbol_elements <=
+                fifi::length_to_elements<field_type>(
+                    SuperCoder::symbol_length()));
+
+            for(uint32_t i = SuperCoder::symbols(); i > 0; --i)
+            {
+                print_symbol(out, symbol_elements, i-1);
+            }
+
+            out << std::endl;
+        }
+
+        /// Print the i'th symbol
+        /// @param out The output stream to print to
+        /// @param symbol_index index of symbol to print
+        void print_symbol(std::ostream& out, uint32_t symbol_index)
+        {
+            uint32_t symbol_elements =
+                fifi::length_to_elements<field_type>(
+                    SuperCoder::symbol_length());
+
+            print_symbol(out, symbol_elements, symbol_index);
+        }
+
+        /// Print the i'th symbol
+        /// @param out The output stream to print to
+        /// @param symbol_elements number of elements in a symbol
+        /// @param symbol_index index of symbol to print
+        void print_symbol(std::ostream& out, uint32_t symbol_elements,
+                          uint32_t symbol_index)
+        {
+            assert(symbol_index < SuperCoder::symbols());
+
+            assert(symbol_elements <=
+                fifi::length_to_elements<field_type>(
+                    SuperCoder::symbol_length()));
+
+            value_type* symbol =
+                SuperCoder::symbol_value(symbol_index);
+
+            for(uint32_t j = symbol_elements; j > 0; --j)
+            {
+                value_type value =
+                    fifi::get_value<field_type>(symbol, j-1);
+
+                out << (uint32_t)value << " ";
+            }
+
+            out << std::endl;
+        }
+    };
+}
